d1925091ce485cbe426078f73c936293ace38c66
Danial Bakirov <vasya@pupkin.ru>
2024-12-15 03:01:10 +0300

    добавил экземпляр сервиса и репозитория

---
+++ server/auth_server/src/main/java/org/example/auth_server/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package org.example.auth_server.repository;
+
+import org.example.auth_server.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findUserByEmail(User user);
+}
---
+++ server/auth_server/src/main/java/org/example/auth_server/service/RegAdminService.java
@@ -0,0 +1,8 @@
+package org.example.auth_server.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RegAdminService {
+
+}
