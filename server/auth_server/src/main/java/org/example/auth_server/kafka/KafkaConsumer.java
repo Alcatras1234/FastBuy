d54cba97091058776401bd38b061c537ab26364f
Danial Bakirov <vasya@pupkin.ru>
2025-02-05 00:34:12 +0300

    delete kafka

--- server/auth_server/src/main/java/org/example/auth_server/kafka/KafkaConsumer.java
+++ server/auth_server/src/main/java/org/example/auth_server/kafka/KafkaConsumer.java
@@ -1,29 +1,29 @@
-package org.example.auth_server.kafka;
-
-import lombok.extern.log4j.Log4j2;
-import org.example.auth_server.utils.JWTUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.stereotype.Service;
-
-@Service
-@Log4j2
-public class KafkaConsumer {
-    private final KafkaTemplate<String, String> kafkaTemplate;
-
-    @Autowired
-    public KafkaConsumer(KafkaTemplate<String, String> kafkaTemplate) {
-        this.kafkaTemplate = kafkaTemplate;
-    }
-
-    @KafkaListener(topics = "validataion_group", groupId = "auth_service")
-    public void validateToken(String message) {
-        log.info("Получил сообщение" + message);
-        boolean isValid = JWTUtils.validateToken(message);
-        String response = isValid ? "VALID" : "INVALID";
-        kafkaTemplate.send("token-validation-response", response);
-        log.info("Отправил ответ" + response);
-    }
-}
-
+//package org.example.auth_server.kafka;
+//
+//import lombok.extern.log4j.Log4j2;
+//import org.example.auth_server.utils.JWTUtils;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.kafka.annotation.KafkaListener;
+//import org.springframework.kafka.core.KafkaTemplate;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//@Log4j2
+//public class KafkaConsumer {
+//    private final KafkaTemplate<String, String> kafkaTemplate;
+//
+//    @Autowired
+//    public KafkaConsumer(KafkaTemplate<String, String> kafkaTemplate) {
+//        this.kafkaTemplate = kafkaTemplate;
+//    }
+//
+//    @KafkaListener(topics = "validataion_group", groupId = "auth_service")
+//    public void validateToken(String message) {
+//        log.info("Получил сообщение" + message);
+//        boolean isValid = JWTUtils.validateToken(message);
+//        String response = isValid ? "VALID" : "INVALID";
+//        kafkaTemplate.send("token-validation-response", response);
+//        log.info("Отправил ответ" + response);
+//    }
+//}
+//
--- server/auth_server/src/main/java/org/example/auth_server/kafka/KafkaProducer.java
+++ server/auth_server/src/main/java/org/example/auth_server/kafka/KafkaProducer.java
@@ -1,18 +1,18 @@
-package org.example.auth_server.kafka;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.stereotype.Service;
-
-@Service
-public class KafkaProducer {
-
-    private static final String TOPIC = "validataion_group";
-
-    @Autowired
-    private KafkaTemplate<String, String> kafkaTemplate;
-
-    public void sendMessage(String key, String message) {
-        kafkaTemplate.send(TOPIC, key, message);
-    }
-}
+//package org.example.auth_server.kafka;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.kafka.core.KafkaTemplate;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class KafkaProducer {
+//
+//    private static final String TOPIC = "validataion_group";
+//
+//    @Autowired
+//    private KafkaTemplate<String, String> kafkaTemplate;
+//
+//    public void sendMessage(String key, String message) {
+//        kafkaTemplate.send(TOPIC, key, message);
+//    }
+//}
