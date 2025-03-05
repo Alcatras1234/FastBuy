14a7c3ad2e24b1d66105700991f1a6e97ebad560
Danial Bakirov <vasya@pupkin.ru>
2025-03-05 17:52:00 +0300

    ///

--- server/auth_server/src/main/java/org/example/auth_server/service/EmailService.java
+++ server/auth_server/src/main/java/org/example/auth_server/service/EmailService.java
@@ -33,7 +33,7 @@ public class EmailService {
             log.info("Sending password " + password);
             String host = "smtp.gmail.com";
             String validateToken = JWTUtils.generateValidateToken(email);
-            String link = "http://46.8.225.59/valid-email/?token=" + validateToken;
+            String link = "http://localhost:8080/valid-email/?token=" + validateToken;
 
             properties = new Properties();
             properties.put("mail.debug", "true");
